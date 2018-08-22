58a5b655871a32c7634d1ad10d3348e874c6af20
“wanghu6652” <wanghu6652@126.com>
2018-08-22 17:08:07 +0800

    7.1.2 request all memory
    Signed-off-by: “wanghu6652” <wanghu6652@126.com>

--- 7.1_malloc_memory.c
+++ 7.1_malloc_memory.c
@@ -3,20 +3,31 @@
 #include <stdio.h>
 
 #define A_MEGABYTE (1024*1024)
+#define PHY_MEM_MEGS 1024
 
 int main()
 {
 	char *some_memory;
-	int megabyte  = A_MEGABYTE;
-	int exit_code = EXIT_FAILURE;
-
-	some_memory = (char *)malloc(megabyte);
-	if(some_memory != NULL){
-		sprintf(some_memory, "Hello world");
-		printf("%s", some_memory);
-		exit_code = EXIT_SUCCESS;
-	}
-
-	exit(exit_code);
+	size_t size_to_allocate = A_MEGABYTE;
+//	int megabyte  = A_MEGABYTE;
+//	int exit_code = EXIT_FAILURE;
+	int megs_obtained = 0;
 	
+	while(megs_obtained < (PHY_MEM_MEGS * 2)){
+//		some_memory = (char *)malloc(megabyte);
+		some_memory = (char *)malloc(size_to_allocate);
+
+		if(some_memory != NULL){
+			megs_obtained++;
+			sprintf(some_memory, "Hello world");
+//			printf("%s", some_memory);
+			printf("%s - now allocated %d Megabytes\n", some_memory, megs_obtained);
+//			exit_code = EXIT_SUCCESS;
+		}
+		else{
+			exit(EXIT_FAILURE);
+		}
+	}
+//	exit(exit_code);
+	exit(EXIT_SUCCESS);	
 }
