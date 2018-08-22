4b9eb856f966e26dffb7f4dc91107528198c8b53
“wanghu6652” <wanghu6652@126.com>
2018-08-22 21:40:44 +0800

    7.1.4 memory5b: reading content from 0 address is not allowed.
    Signed-off-by: “wanghu6652” <wanghu6652@126.com>

--- 7.1.4_memory5a.c
+++ 7.1.4_memory5a.c
@@ -4,9 +4,11 @@
 
 int main()
 {
-	char *some_memory = (char *)0;
-
-	printf("A read from null %s\n", some_memory);
-	sprintf(some_memory, "Hello world");
+//	char *some_memory = (char *)0;
+	char z = *(char *)0;
+	printf("I read from location zero\n");
+	
+//	printf("A read from null %s\n", some_memory);
+//	sprintf(some_memory, "Hello world");
 	exit(EXIT_SUCCESS);
 }
