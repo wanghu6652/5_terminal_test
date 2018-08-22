78ea6516009765e5acd5156960b96f5db296b4e5
“wanghu6652” <wanghu6652@126.com>
2018-08-22 22:01:08 +0800

    7.1.5 memory6: free memory
    Signed-off-by: “wanghu6652” <wanghu6652@126.com>

--- 7.1_malloc_memory.c
+++ 7.1_malloc_memory.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+//#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,33 +10,35 @@ int main()
 {
 	char *some_memory;
 //	size_t size_to_allocate = A_MEGABYTE;
-	int size_to_allocate = ONE_K;
+//	int size_to_allocate = ONE_K;
 //	int megabyte  = A_MEGABYTE;
-//	int exit_code = EXIT_FAILURE;
-	int megs_obtained = 0;
-	int ks_obtained = 0;
-
+	int exit_code = EXIT_FAILURE;
+//	int megs_obtained = 0;
+//	int ks_obtained = 0;
+	some_memory = (char *)malloc(ONE_K);
 //	while(megs_obtained < (PHY_MEM_MEGS * 2)){
 //		some_memory = (char *)malloc(megabyte);
-	while(1){
-		for(ks_obtained = 0; ks_obtained < 1024; ks_obtained++){
-			some_memory = (char *)malloc(size_to_allocate);
-			if(some_memory == NULL) exit(EXIT_FAILURE);
-			sprintf(some_memory, "Hello world");
-		}
-		megs_obtained++;
-		printf("Now allocated %d Megabytes\n", megs_obtained);
-//		if(some_memory != NULL){
+//	while(1){
+//		for(ks_obtained = 0; ks_obtained < 1024; ks_obtained++){
+//			some_memory = (char *)malloc(size_to_allocate);
+//			if(some_memory == NULL) exit(EXIT_FAILURE);
+//			sprintf(some_memory, "Hello world");
+//		}
+//		megs_obtained++;
+//		printf("Now allocated %d Megabytes\n", megs_obtained);
+		if(some_memory != NULL){
+			free(some_memory);
+			printf("Memory allocated and freed again\n");
 //			megs_obtained++;
 //			sprintf(some_memory, "Hello world");
 //			printf("%s", some_memory);
 //			printf("%s - now allocated %d Megabytes\n", some_memory, megs_obtained);
-//			exit_code = EXIT_SUCCESS;
+			exit_code = EXIT_SUCCESS;
 //		}
 //		else{
 //			exit(EXIT_FAILURE);
 //		}
 	}
-//	exit(exit_code);
-	exit(EXIT_SUCCESS);	
+	exit(exit_code);
+//	exit(EXIT_SUCCESS);	
 }
